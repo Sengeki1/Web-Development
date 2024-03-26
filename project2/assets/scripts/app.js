671da225beda9dce2231f0ca60b4c05d98e12173
Marco Soares <106749775+Sengeki1@users.noreply.github.com>
2024-03-26 10:44:43 -0100

    heal event

--- project2/assets/scripts/app.js
+++ project2/assets/scripts/app.js
@@ -4,6 +4,7 @@ let valorActualVidaM = maxVida
 let valorActualVidaP = maxVida
 let healVida = 10
 let strongAttack = 20
+let lifeRestored = 0;
 
 const ataque = () => {
 	const perda = dealMonsterDamage(VALOR_ATAQUE)
@@ -12,7 +13,6 @@ const ataque = () => {
 	const perdaPlayer = dealPlayerDamage(VALOR_ATAQUE)
 	valorActualVidaP -= perdaPlayer
 
-	
 	if (valorActualVidaM < 0 && valorActualVidaP > 0) {
 		alert('venceu!')
 	} else if (valorActualVidaP <= 0 && valorActualVidaM > 0) {
@@ -27,15 +27,11 @@ const heal = () => {
 	if (choose === 1) {
 		increasePlayerHealth(healVida)
 		valorActualVidaP += heal
+		console.log(valorActualVidaP)
 	} else {
 		increaseMonsterHealth(healVida)
 		valorActualVidaM += heal
 	}
-}
-
-const strongAttackk = () => {
-	const perda = dealMonsterDamage(strongAttack)
-	valorActualVidaM -= perda
 
 	if (valorActualVidaM < 0 && valorActualVidaP > 0) {
 		alert('venceu!')
@@ -46,7 +42,24 @@ const strongAttackk = () => {
 	}
 }
 
+const strongAttackk = () => {
+	const choose = Math.floor(Math.random() * 2)
+	if (choose === 1) {
+		const perda = dealMonsterDamage(strongAttack)
+		valorActualVidaM -= perda
+	} else {
+		const perdaPlayer = dealPlayerDamage(strongAttack)
+		valorActualVidaP -= perdaPlayer
+	}
 
+	if (valorActualVidaM < 0 && valorActualVidaP > 0) {
+		alert('venceu!')
+	} else if (valorActualVidaP <= 0 && valorActualVidaM > 0) {
+		alert('perdeu!')
+	} else if (valorActualVidaP <= 0 && valorActualVidaM <= 0) {
+		alert('impate!')
+	}
+}
 
 attackBtn.addEventListener('click', ataque)
 healBtn.addEventListener('click', heal)
