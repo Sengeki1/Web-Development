2209e889e21d6c04da4a16a2a16f6e9efe112a57
Marco Soares <106749775+Sengeki1@users.noreply.github.com>
2024-04-04 12:04:37 -0100

    finished delete movie

--- s3a1code/assets/scripts/app.js
+++ s3a1code/assets/scripts/app.js
@@ -1,8 +1,11 @@
 const addFilmeBtn = document.querySelector('header button')
 const addFilme = document.getElementById('add-modal') 
 const cancelBtn = document.querySelector('.btn--passive')
+const cancelDeleteBtn = document.querySelector('#delete-modal .btn--passive')
 const backdrop = document.getElementById('backdrop')
+const deleteMovie = document.getElementById('delete-modal')
 const btnConfirmAdd = cancelBtn.nextElementSibling
+const btnConfirmDelete = cancelDeleteBtn.nextElementSibling
 
 const title = document.getElementById('title')
 const image = document.getElementById('image-url')
@@ -10,6 +13,8 @@ const classification = document.getElementById('rating')
 const listMovies = document.getElementById('movie-list')
 
 const msgText = document.getElementById('entry-text')
+
+let movieItem
 
 const filmes = []
 
@@ -33,17 +38,18 @@ const updateList = () => {
     
     const listItems = filmes.map(item => {
         return `
-        <div id="movie-item">
+        <div id="movie-item" data-id="${item.id}">
             <img src='${item.image}'>
             <div id="movie-details">
                 <h3>${item.title}</h3>
-                <p>${item.classification} / 4</p>
+                <p>${item.classification} / 5</p>
             </div>
         </div>
         `
     });
     
     listMovies.innerHTML = listItems.join('')
+
 }
 
 const btnConfirmAddHandler = () => {
@@ -53,9 +59,9 @@ const btnConfirmAddHandler = () => {
 	const ClassField = classification.value
 
 	if (titleField.trim() === '' || imageField.trim() === '' || ClassField.trim() === '') {
-		alert('precisa prencher os dados')
+        alert('precisa prencher os dados')
 		return
-	}
+    }
 	
 	const novoFilme = {
 		id: Math.random().toString(),
@@ -69,9 +75,45 @@ const btnConfirmAddHandler = () => {
 
 	/* Atualizar Lista */
 	updateList()
-	console.log(filmes)
+}
+
+const btnDeleteMovie = (event) => {
+
+    if (event.target.id === ('movie-item')) {
+        deleteMovie.classList.add('visible')
+        backdrop.classList.add('visible')
+    }
+
+    movieItem = event.target.closest('#movie-item')
+    
+}
+
+const cancelDeleteMovie = () => {
+    deleteMovie.classList.remove('visible')
+    backdrop.classList.remove('visible')
+}
+
+const confirmDeleteMovie = () => {
+    deleteMovie.classList.remove('visible')
+    backdrop.classList.remove('visible')
+
+    if (movieItem) {
+        const movieId = movieItem.dataset.id // access the data attribute (id)
+        const index = filmes.findIndex(movie => movie.id === movieId) // Find the index of the movie object in the filmes array 
+        if (index !== -1) { // If movie object is found
+            filmes.splice(index, 1) // Remove the movie object from the array
+            updateList()
+        }
+    }
 }
 
 addFilmeBtn.addEventListener('click', addMovie)
 cancelBtn.addEventListener('click', cancelMovie)
 btnConfirmAdd.addEventListener('click', btnConfirmAddHandler)
+listMovies.addEventListener('click', btnDeleteMovie)
+cancelDeleteBtn.addEventListener('click', cancelDeleteMovie)
+btnConfirmDelete.addEventListener('click', confirmDeleteMovie)
+
+
+
+
