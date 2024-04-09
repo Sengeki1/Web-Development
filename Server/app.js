72a0595d225d8fbe6dafe3f9f2e0a0c660e182ec
Marco Soares <106749775+Sengeki1@users.noreply.github.com>
2024-04-09 17:09:05 -0100

    Add files via upload

--- Server/app.js
+++ Server/app.js
@@ -1,21 +1,23 @@
+const express = require('express')
 const http = require('http') // module http
 
-const hostname = '127.0.0.1'
+const app = express()
+
+// MiddleWare Functions
+app.use((request, response, next) => {
+    console.log('no primeiro middleware')
+    next()
+})
+
+app.use((request, response, next) => {
+    console.log('no segundo middleware')
+    response.send('<h1>Ola do servidor express</h1>')
+})
+
+const server = http.createServer(app)
 const port = 5000
+const host = '127.0.0.1'
 
-const server = http.createServer( (request, response) => {
-    
-    console.log(request.url, request.method, request.headers)
-
-    response.setHeader('Content-Type', 'text/html')
-    response.write('<html><head><title><Resposta do Servidor</title></head>')
-    response.write('<body><h1>Ola do Servidor Web com NodeJS!</h1></body>')
-    response.write('html')
-    response.end()
-
-    }
-)
-
-server.listen(port, hostname, () => {  // listen for a request 
-    console.log(`Servidor a rodar em http://${hostname}:${port}/`)
+server.listen(port, host, () => {  // listen for a request 
+    console.log(`Servidor a rodar em http://${host}:${port}/`)
 })
