792ad7f02a268d75847e720ce0944ba41b5b15b5
Sengeki1 <gamersengeki@gmail.com>
2024-04-16 10:36:20 -0100

    Manipuladores de rota

--- Roteamento/app.js
+++ Roteamento/app.js
@@ -4,12 +4,19 @@ var bodyParser = require('body-parser')
 
 const app = express()
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res, next) {
     res.send('<html><body><form method="POST">' +  
     'First Name: <input type="text" name="first_name"><br>' +  
     'Last Name: <input type="text" name="last_name">' +
     '<input type="submit" value="Submit">' +
     '</form></body></html>')
+})
+
+app.get('/route1', (request, response, next) => {
+    console.log('Hello') 
+    next()   
+}, (req, res) => {
+    res.send('B diz ola')
 })
 
 app.get(/a/, (req, res) => {
@@ -20,6 +27,7 @@ app.post('/', function (req, res) {
     res.send('Got a POST request');
 });
 
+/*
 app.put('/user', function(request, response) {
     response.send('Hello World!')
 })
@@ -27,6 +35,7 @@ app.put('/user', function(request, response) {
 app.delete('/user', function (req, res) {
     res.send('Got a DELETE request at /user');
 });
+*/
 
 const port = 5000
 const host = '127.0.0.1'
