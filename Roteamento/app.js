15c12c8af2d66d2f0177aebc01ee75d8737717d5
Sengeki1 <gamersengeki@gmail.com>
2024-04-16 10:41:34 -0100

    Manipuladores de rota (Matrizes)

--- Roteamento/app.js
+++ Roteamento/app.js
@@ -12,16 +12,18 @@ app.get('/', function (req, res, next) {
     '</form></body></html>')
 })
 
-app.get('/route1', (request, response, next) => {
+app.get('/example/a', (request, response, next) => {
     console.log('Hello') 
     next()   
 }, (req, res) => {
-    res.send('B diz ola')
+    res.send('A diz ola')
 })
 
+/*
 app.get(/a/, (req, res) => {
     res.send('/a/')
 })
+*/
 
 app.post('/', function (req, res) {
     res.send('Got a POST request');
@@ -36,6 +38,23 @@ app.delete('/user', function (req, res) {
     res.send('Got a DELETE request at /user');
 });
 */
+
+// Matriz de funções
+const cb0 = (req, res, next) => {
+    console.log('CB0')
+    next()
+}
+const cb1 = (req, res, next) => {
+    console.log('CB1')
+    next()
+}
+app.get('/example/b', [cb0, cb1], (req, res, next) => {
+    console.log('a resposta sera enviada pela proxima função')
+    next()
+}, (req, res) => {
+    res.send('B diz olá')
+})
+
 
 const port = 5000
 const host = '127.0.0.1'
